defa18d6d13d12d56e9b6e4d576e20adeaedb4dd
amour <amrfahmy57@hotmail.com>
2018-02-25 01:20:40 +0200

    final_game

--- src/hang.java
+++ src/hang.java
@@ -12,7 +12,7 @@ import java.util.*;
 	}catch(Exception e) {System.out.println("Error");}
 	}
 	
-	public void readFile()
+	public String readFile()
 	{
 		int i=0;
 		String words[] = new String[1000];
@@ -25,6 +25,7 @@ import java.util.*;
 		Random rand = new Random(); 
 		int value = rand.nextInt(1000); 
 		System.out.println(words[value]);
+		return words[value];
 		
 	}
 	
@@ -35,14 +36,77 @@ import java.util.*;
 	
 	
 }
+ 
+ class game {
+	 
+	 public void show(String word)
+	 {
+		 int life=3,right=0;
+		 Scanner reader= new Scanner(System.in);
+		 int length = word.length();
+		 Boolean[] exist = new Boolean[length];
+		 Arrays.fill(exist, Boolean.FALSE);
+		 char answer;
+		 int j=1;
+		
+		 while(j==1&&life!=0&&right!=length) {
+			 
+			 int flag=0,temp=0;
+			 System.out.println("Lifes : "+life);
+		 for (int i=0;i<length;i++)
+		 {
+			 if(exist[i]==true)
+			 {
+				 System.out.print(word.charAt(i)+" ");
+				 
+			 }
+			 else
+			 {
+				 System.out.print("_ ");
+			 }
+			 
+			 
+		 }
+		 answer = reader.next().charAt(0);
+		 for (int i=0;i<length;i++)
+		 {
+			 if(answer==word.charAt(i))
+			 {
+				 if(exist[i]==true) {flag=2;}
+				 else {
+				 exist[i]=true;
+				 flag=1;
+				 temp++;}
+			 }
+			 
+		 }
+		 if(flag==0) {System.out.println("Wrong answer"); life--;}
+		 else if(flag==2)System.out.println("You entered this letter before");
+		 else {System.out.println("Right answer");
+		 
+		right+=temp;
+	 }
+	 }
+		 if(life==0)System.out.print("GAME OVER. THE WORD WAS : "+word);
+		 else System.out.print("Great you won. The word was : "+word);
+	 
+	 
+	 
+ }
+	 
+	
+ }
 
 public class hang {
 	public static void main(String [] args) {
 	
+	String word;
 	read r= new read();
 	r.openFile();
-	r.readFile();
+	word=r.readFile();
 	r.close();
+	game g= new game();
+	g.show(word);
 	}
 	
 
